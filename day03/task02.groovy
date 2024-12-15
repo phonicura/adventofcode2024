58b196638751c65e27786f3fb9f376bb68d5902a
phonicura <account@lukasmundt.de>
2024-12-16 00:36:41 +0100

    Day 3

---
+++ day03/task02.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+
+def reader = System.in.newReader()
+def line = ''
+
+def input = ''
+while ((line = reader.readLine()) != null) {
+  input += line
+}
+
+def result = input
+  .replaceAll(/don't\(\).*?do\(\)/, '')
+  .findAll(/mul\([0-9]+,[0-9]+\)/)
+  .collect {
+    def pair = it.findAll(/[0-9]+/).collect{ it as int }
+    return pair[0] * pair[1]
+  }.sum()
+
+println(result)
