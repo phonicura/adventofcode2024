5f75fbfcce1460aad24aa958b8822cce5805918e
phonicura <account@lukasmundt.de>
2024-12-15 23:07:51 +0100

    Day 1

---
+++ day01/task01.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+def reader = System.in.newReader()
+def line = ''
+
+def pairs = []
+while ((line = reader.readLine()) != null) {
+  def pair = line.split(' +').collect{ it as int }
+  pairs.add(pair)
+}
+
+def totalDistance = pairs
+  .transpose().collect{ it.sort() }
+  .transpose().collect{ Math.abs(it[0] - it[1]) }
+  .sum()
+
+println(totalDistance)
---
+++ day01/task02.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+def reader = System.in.newReader()
+def line = ''
+
+def pairs = []
+while ((line = reader.readLine()) != null) {
+  def pair = line.split(' +').collect{ it as int }
+  pairs.add(pair)
+}
+
+def lists = pairs.transpose().collect{ it.sort() }
+
+def similarityScore = lists[0]
+  .collect{ it * lists[1].count(it) }
+  .sum()
+
+println(similarityScore)
