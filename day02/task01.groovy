5e401e87a8956f60bc0886b12e1aec5125f300f9
phonicura <account@lukasmundt.de>
2024-12-15 23:48:43 +0100

    Day 2

---
+++ day02/task01.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/env groovy
+
+boolean isSafe(List<Integer> report) {
+  def reportSorted = report.sort(false)
+
+  // Check for double values
+  if (report != report.unique()) {
+    return false
+  }
+
+  // Check if list is sorted
+  if (![reportSorted, reportSorted.reverse()].contains(report)) {
+    return false
+  }
+
+  // Check for max distance
+  def maxDistance = [report, report.subList(1, report.size())]
+    .transpose()
+    .collect{ Math.abs(it[0] - it[1]) }
+    .max()
+
+  return maxDistance <= 3
+}
+
+def reader = System.in.newReader()
+def line = ''
+
+def reports = []
+while ((line = reader.readLine()) != null) {
+  def report = line.split(' +').collect{ it as Integer }
+  reports.add(report)
+}
+
+def safeCount = reports
+  .collect { isSafe(it) }
+  .collect{ it.compareTo(false) }
+  .sum()
+
+println(safeCount)
